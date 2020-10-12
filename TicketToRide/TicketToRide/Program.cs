95bdf4c07519f5a4969b91e978a44f8d83c01091
Darko Panchevski <darko.panchevski@gmail.com>
2020-10-12 20:00:10 +0200

    Inital commit + .gitignore + .gitattributes

---
+++ TicketToRide/TicketToRide/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicketToRide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
